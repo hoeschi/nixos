b60bec314ba591a5330cd626c1c8f876df62a361
System administrator <hoeschbj+github@proton.me>
2026-02-09 21:34:59 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Die absolut zusammengeklaute Flake Config von bhoesch";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+  {
+
+    nixosConfigurations = {
+
+      # Test-Laptop
+      echo = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+            ./hosts/echo/configuration.nix
+        ];
+      };
+    };
+
+  };
+}
