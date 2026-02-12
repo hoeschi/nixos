11dba33450905c96547f80f1588282fa73850005
hoeschi <hoeschbj@web.de>
2026-02-12 19:27:16 +0100

    Update steam.nix

--- modules/home/gaming/steam.nix
+++ modules/home/gaming/steam.nix
@@ -1,16 +1,16 @@
 {config, pkgs, lib, ...}:
 {
   options = {
+
     modules.gaming.steam.enable = lib.mkEnableOption "Steam setup";
+    
   };
+
   config = lib.mkIf config.modules.gaming.steam.enable {
     
     home.packages = with pkgs; [
       steam
     ];
-
-    # Proton für Windows-Spiele
-    protontricksSupport = true;
 
   };
 }
