ada10b2b0ba94c8569093c0834a5d1235f01a91e
hoeschi <hoeschbj@web.de>
2026-02-13 13:42:02 +0100

    Update vscode.nix

--- modules/home/development/vscode.nix
+++ modules/home/development/vscode.nix
@@ -8,23 +8,27 @@
     programs.vscode = {
       
       enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        ms-python.python
-        #ms-toolsai.jupyter
-        #ms-vscode.cpptools
-        #ms-vscode.cmake-tools
+
+      profiles.default = {
+        name = "default";
+        extensions = with pkgs.vscode-extensions; [
+          ms-python.python
+          #ms-toolsai.jupyter
+          #ms-vscode.cpptools
+          #ms-vscode.cmake-tools
         #ms-vscode.cpptools-extension-pack
         #ms-vscode.cpptools-themes
         #ms-vscode.cmake-tools-extension-pack
         #ms-vscode.cmake-tools-themes
-      ];
-      userSettings = {
-        "editor.fontSize" = 14;
-        "editor.tabSize" = 4;
-        "editor.insertSpaces" = true;
-        "editor.detectIndentation" = false;
-        "files.autoSave" = "afterDelay";
-        "files.autoSaveDelay" = 1000;
+        ];
+        userSettings = {
+          "editor.fontSize" = 14;
+          "editor.tabSize" = 4;
+          "editor.insertSpaces" = true;
+          "editor.detectIndentation" = false;
+          "files.autoSave" = "afterDelay";
+          "files.autoSaveDelay" = 1000;
+        };
       };
     };
 
