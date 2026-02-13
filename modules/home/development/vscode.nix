a484e2eaa93e4ce5ac0a222eb6d1af3c02fae09c
hoeschi <hoeschbj@web.de>
2026-02-13 13:43:51 +0100

    Update vscode.nix

--- modules/home/development/vscode.nix
+++ modules/home/development/vscode.nix
@@ -9,26 +9,24 @@
       
       enable = true;
 
-      profiles.default = {
-        name = "default";
-        extensions = with pkgs.vscode-extensions; [
-          ms-python.python
-          #ms-toolsai.jupyter
-          #ms-vscode.cpptools
-          #ms-vscode.cmake-tools
-        #ms-vscode.cpptools-extension-pack
-        #ms-vscode.cpptools-themes
-        #ms-vscode.cmake-tools-extension-pack
-        #ms-vscode.cmake-tools-themes
-        ];
-        userSettings = {
-          "editor.fontSize" = 14;
-          "editor.tabSize" = 4;
-          "editor.insertSpaces" = true;
-          "editor.detectIndentation" = false;
-          "files.autoSave" = "afterDelay";
-          "files.autoSaveDelay" = 1000;
-        };
+      name = "default";
+      extensions = with pkgs.vscode-extensions; [
+        ms-python.python
+        #ms-toolsai.jupyter
+        #ms-vscode.cpptools
+        #ms-vscode.cmake-tools
+      #ms-vscode.cpptools-extension-pack
+      #ms-vscode.cpptools-themes
+      #ms-vscode.cmake-tools-extension-pack
+      #ms-vscode.cmake-tools-themes
+      ];
+      userSettings = {
+        "editor.fontSize" = 14;
+        "editor.tabSize" = 4;
+        "editor.insertSpaces" = true;
+        "editor.detectIndentation" = false;
+        "files.autoSave" = "afterDelay";
+        "files.autoSaveDelay" = 1000;
       };
     };
 
