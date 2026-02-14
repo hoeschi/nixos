792a73f0f80f63c099dc8fd79c8831e71b0cb12a
hoeschi <hoeschbj@web.de>
2026-02-14 10:36:51 +0100

    added gaia host

---
+++ hosts/gaia/configuration.nix
@@ -0,0 +1,153 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+
+      ./../../modules/system/sddm.nix
+    ];
+
+   # NixOS Settings with Home Manager
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  # Bootloader.
+  boot.loader = {
+
+    systemd-boot.enable = false;
+    efi.canTouchEfiVariables = true;
+
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+
+      useOSProber = true;
+    };
+  };
+
+
+  networking.hostName = "echo"; # Define your hostname.
+  
+  # Enable networking
+  networking.networkmanager.enable = true;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # ------------------------------ #
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  #services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  #services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # ------------------------------ #
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.bhoesch = {
+    isNormalUser = true;
+    description = "Bjarne Hösch";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  #programs.home-manager.enable = true;
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      bhoesch = import ./home.nix;
+    };
+  };
+
+  # Install System packages.
+  programs = {
+    #firefox.enable = true;
+    #git.enable = true;
+    #github-desktop.enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+    neovim
+    github-desktop
+    git
+  ];
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
---
+++ hosts/gaia/home.nix
@@ -0,0 +1,26 @@
+{config, inputs, pkgs, ...}:
+
+{
+
+  imports = [
+    ../../modules/home
+  ];
+
+  programs.home-manager.enable = true;
+
+  home = {
+    username = "bhoesch";
+    homeDirectory = "/home/bhoesch";
+
+    stateVersion = "25.11";
+  };
+
+  #enable Modules
+  modules = {
+    browser.firefox.enable = true;
+    desktop.kdeplasma.enable = true;
+    development.vscode.enable = true;
+    gaming.steam.enable = true;
+  };
+
+}
