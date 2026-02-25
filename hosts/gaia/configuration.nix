8d93ae26c17d3224b067fbdef19d68fb70865639
hoeschi <hoeschbj+github@proton.me>
2026-02-25 10:19:05 +0100

    refactored config

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -12,6 +12,7 @@
 
       ./../../modules/system/sddm.nix
       ./../../modules/system/games.nix
+      ./../../modules/system/flatpack.nix
     ];
 
    # NixOS Settings with Home Manager
@@ -45,76 +46,79 @@
 
   # ------------------------------ #
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "de_DE.UTF-8";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "de_DE.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  #services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  #services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "";
-  };
-
-    #xdg.portal = {
-        #enable = true;
-        #extraPortals = [ pkgs.xdg-desktop-portal-kde ];
-    #};
-
-
-  # Configure console keymap
-  console.keyMap = "de";
-
-  # ------------------------------ # Hardware und Treiber
-
-  hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "de_DE.UTF-8";
+        LC_IDENTIFICATION = "de_DE.UTF-8";
+        LC_MEASUREMENT = "de_DE.UTF-8";
+        LC_MONETARY = "de_DE.UTF-8";
+        LC_NAME = "de_DE.UTF-8";
+        LC_NUMERIC = "de_DE.UTF-8";
+        LC_PAPER = "de_DE.UTF-8";
+        LC_TELEPHONE = "de_DE.UTF-8";
+        LC_TIME = "de_DE.UTF-8";
     };
-  };
+
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    #services.xserver.enable = true;
+
+    # Enable the KDE Plasma Desktop Environment.
+    #services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
 
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    #  Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "de";
+        variant = "";
+    };
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [ 
+            kdePackages.xdg-desktop-portal-kde
+            xdg-desktop-portal-gtk
+        ];
+    };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+
+    # Configure console keymap
+    console.keyMap = "de";
+
+    # ------------------------------ # Hardware und Treiber
+
+    hardware = {
+        graphics = {
+        enable = true;
+        enable32Bit = true;
+        };
+    };
+
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+    };
 
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -146,6 +150,7 @@
   services = {
 
     ratbagd.enable = true; # mice configuration service(needed for piper)
+    input-remapper.enable = true;
   
   };
 
@@ -177,7 +182,7 @@
         withVencord = true;
     })
 
-    
+    mullvad-vpn
 
     whatsapp-electron
     libreoffice
@@ -191,6 +196,7 @@
 
     evtest # for debbuging of input signals
     xev
+    input-remapper
 
   ];
 
