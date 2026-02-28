fe44d1bade758487bc597817a30ed4a188e7c9cd
hoeschi <hoeschbj+github@proton.me>
2026-02-28 01:52:18 +0100

    enabled guitarix and helvum

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -113,7 +113,7 @@
         alsa.support32Bit = true;
         pulse.enable = true;
         # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
+        jack.enable = true;
 
         # use the example session manager (no others are packaged yet so this is enabled by default,
         # no need to redefine it in your config for now)
@@ -175,6 +175,8 @@
 
     neovim
     fastfetch
+    wget
+    helvum #Pipewire configuration with gui
 
     github-desktop
     git
@@ -200,6 +202,9 @@
     evtest # for debbuging of input signals
     xev
     input-remapper
+
+    # Guitare
+    guitarix
 
   ];
 
