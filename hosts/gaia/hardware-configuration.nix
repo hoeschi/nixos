358993581ec470b0007e93317c3e481e3b52a7ca
hoeschi <hoeschbj+github@proton.me>
2026-05-25 12:06:29 +0200

    added kernel Modul for external dvd drive recognision

--- hosts/gaia/hardware-configuration.nix
+++ hosts/gaia/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ "amdgpu"];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "sg"];
   boot.extraModulePackages = [ ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   
