564fa636122914fc0dccf52eae403617d91c36cf
hoeschi <hoeschbj+github@proton.me>
2026-08-12 22:53:29 +0200

    added xdg.nix

---
+++ modules/system/xdg.nix
@@ -0,0 +1,22 @@
+{ ... }:
+{
+  # XDG Base Directory Specification – systemweit für alle Sessions.
+  # Wird via PAM (/etc/pam/environment) gesetzt, gilt also auch für
+  # grafische Logins über SDDM, nicht nur für Login-Shells.
+  environment.sessionVariables = {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+  };
+
+  # Verzeichnisse deklarativ anlegen, statt sich darauf zu verlassen,
+  # dass jedes Programm das selbst tut. %h = Home des jeweiligen Users.
+  systemd.user.tmpfiles.rules = [
+    "d %h/.cache        0700 - - -"
+    "d %h/.config       0755 - - -"
+    "d %h/.local        0755 - - -"
+    "d %h/.local/share  0755 - - -"
+    "d %h/.local/state  0755 - - -"
+  ];
+}
