f3b8117546d33a015648d510e56ea2bfb2f1a478
hoeschi <hoeschbj+github@proton.me>
2026-08-13 12:36:44 +0200

    chenged prompt and added some zsh features

--- modules/home/terminal/zsh.nix
+++ modules/home/terminal/zsh.nix
@@ -3,54 +3,70 @@
   lib,
   ...
 }: {
-
   programs.zsh = {
-    
     enable = true;
     autocd = true;
     enableCompletion = true;
-    dotDir = "${config.home.homeDirectory}/.config/zsh"; # TODO: Add xdg module
+    dotDir = "${config.xdg.configHome}/zsh";
+
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    historySubstringSearch.enable = true;
+
+    history = {
+      path = "${config.xdg.dataHome}/zsh/history";
+      size = 100000;
+      save = 100000;
+      ignoreDups = true;
+      ignoreSpace = true;
+      expireDuplicatesFirst = true;
+    };
 
     shellAliases =
       {
         ls = "eza --icons -l";
         v = "nvim";
         vim = "nvim";
-        rebuild-switch = "nixos-rebuild switch --flake ~/nixos --sudo"; # NOTE: Previously --use-remote-sudo
-        rebuild-switch-upgrade = "nixos-rebuild switch --flake ~/nixos --sudo --upgrade-all";
+        rebuild-switch = "nixos-rebuild switch --flake ~/nixos --sudo";
+        rebuild-switch-upgrade = "nix flake update --flake ~/nixos && nixos-rebuild switch --flake ~/nixos --sudo";
       }
-      // (
-        lib.optionalAttrs config.programs.zoxide.enable
-        {cd = "z";}
-      );
+      // (lib.optionalAttrs config.programs.zoxide.enable {cd = "z";});
 
-    initContent =
-      ''
-        # Enable branches to be displayed in the prompt
-        autoload -Uz vcs_info
-        precmd () { vcs_info }
+    initContent = ''
+      autoload -Uz vcs_info add-zsh-hook
 
-        zstyle ':vcs_info:git:*' formats '(%F{green} %b%f) '
+      zstyle ':vcs_info:*' enable git
+      zstyle ':vcs_info:git:*' formats '(%F{green} %b%f) '
 
-        setopt PROMPT_SUBST
-        PROMPT='%F{blue}%~%f ''${vcs_info_msg_0_}$ '
-      ''
-      + (
-        # TODO: Make based on if yazi is configured
-        lib.optionalString true
-        ''
-          function y() {
-            local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-            yazi "$@" --cwd-file="$tmp"
-            IFS= read -r -d "" cwd < "$tmp"
-            [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-            rm -f -- "$tmp"
-          }
-        ''
-      )
-      + (
-        lib.optionalString config.programs.direnv.enable
-        "eval \"$(direnv hook zsh)\""
-      );
+      # Zeigt an, in welcher devShell wir sind – muss bei jedem Prompt
+      # neu ausgewertet werden, da direnv die Umgebung zur Laufzeit ändert.
+      _dev_shell_prompt() {
+        if [[ -n "$DEV_SHELL_NAME" ]]; then
+          dev_shell_prompt="%F{yellow}[$DEV_SHELL_NAME]%f "
+        elif [[ -n "$IN_NIX_SHELL" ]]; then
+          dev_shell_prompt="%F{yellow}[nix]%f "
+        else
+          dev_shell_prompt=""
+        fi
+      }
+
+      add-zsh-hook precmd vcs_info
+      add-zsh-hook precmd _dev_shell_prompt
+
+      setopt PROMPT_SUBST
+      PROMPT="\''${dev_shell_prompt}%F{blue}%~%f \''${vcs_info_msg_0_}"
+
+      # Systemübersicht beim Start – nur einmal pro Terminalfenster,
+      # nicht erneut in devShells oder verschachtelten Shells.
+      if [[ -n "$KITTY_WINDOW_ID" && -z "$FASTFETCH_SHOWN" ]]; then
+        export FASTFETCH_SHOWN=1
+        fastfetch
+      fi
+    '';
+  };
+
+  programs.yazi = {
+    enable = true;
+    shellWrapperName = "y";
   };
 }
