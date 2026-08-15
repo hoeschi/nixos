c89587ec946844fe04d00f07de1bb681b0859ba5
hoeschi <hoeschbj+github@proton.me>
2026-08-15 12:47:48 +0200

    included den and importtree

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
+    import-tree.url = "github:denful/import-tree";
+    den.url = "github:denful/den";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,11 +26,6 @@
       url = "github:gmodena/nix-flatpak/?ref=latest";
       #inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    #nix-claude-code = {
-    #  url = "github:ryoppippi/nix-claude-code";
-    #  #inputs.nixpkgs.follows = "nixpkgs";
-    #};
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -49,8 +47,7 @@
       echo = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
-            ./hosts/echo/configuration.nix
-            sops-nix.nixosModules.sops
+            ./modules/hosts/echo/configuration.nix
         ];
       };
 
@@ -58,7 +55,7 @@
       gaia = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
-            ./hosts/gaia/configuration.nix
+            ./modules/hosts/gaia/configuration.nix
             sops-nix.nixosModules.sops
         ];
       };
