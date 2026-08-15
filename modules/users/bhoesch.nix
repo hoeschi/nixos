a267d7733cd7edbe5a9cd7f9b4e212c459206563
hoeschi <hoeschbj+github@proton.me>
2026-08-15 15:57:42 +0200

    added user bhoesch file

--- modules/users/bhoesch.nix
+++ modules/users/bhoesch.nix
@@ -3,7 +3,21 @@
     includes = [
       den.batteries.define-user
       den.batteries.primary-user
+
+      (den.batteries.user-shell "zsh")
+
     ];
 
-  }
+    user = {pkgs, ...}: {
+      description = "Bjarne Hösch";
+      uid = 1000;
+      extraGroups = [
+        "docker"
+      ];
+      packages = with pkgs; [
+        kdePackages.kate
+      ];
+    };
+
+  };
 }
