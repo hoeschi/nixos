fcf8975749c40e22fad2182625614d5adb9bdab0
hoeschi <hoeschbj+github@proton.me>
2026-08-15 09:11:02 +0200

    added xdg refactoring

--- hosts/gaia/home.nix
+++ hosts/gaia/home.nix
@@ -2,60 +2,103 @@
 
 {
 
-    imports = [
-        ../../modules/home
+  imports = [
+      ../../modules/home
+  ];
+
+  programs.home-manager.enable = true;
+
+  home = {
+    username = "bhoesch";
+    homeDirectory = "/home/bhoesch";
+
+    stateVersion = "25.11";
+  
+
+    packages = with pkgs; [
+      eza
     ];
 
-    programs.home-manager.enable = true;
-
-    home = {
-        username = "bhoesch";
-        homeDirectory = "/home/bhoesch";
-
-        stateVersion = "25.11";
+    sessionVariables = {
+      DOCKER_CONFIG       = "${config.xdg.configHome}/docker";
+      PLATFORMIO_CORE_DIR = "${config.xdg.dataHome}/platformio";
+      DOTNET_CLI_HOME     = "${config.xdg.dataHome}/dotnet";
+      PYTHON_HISTORY      = "${config.xdg.stateHome}/python_history";
+      #CONDARC             = "${config.xdg.configHome}/conda/condarc";
+      WGETRC              = "${config.xdg.configHome}/wget/wgetrc";
     };
 
-    #home.enableNixpkgsReleaseCheck = false; # use if unmatched nixopkgs and home manager versions
 
-    #nixpkgs = {
-    #  overlays = [
-    #    inputs.nur.overlays.default
-    #  ];
-    #  config = {
-    #    allowUnfree = true;
-    #  };
-    #};
+  };
 
-    home.packages = with pkgs; [
+  #home.enableNixpkgsReleaseCheck = false; # use if unmatched nixopkgs and home manager versions
 
-        eza
+  #nixpkgs = {
+  #  overlays = [
+  #    inputs.nur.overlays.default
+  #  ];
+  #  config = {
+  #    allowUnfree = true;
+  #  };
+  #};
 
-    ];
+  
 
-    home.sessionPath = [
-        "$HOME/.local/bin"
-    ];
+  home.sessionPath = [
+      "$HOME/.local/bin"
+  ];
+  home.preferXdgDirectories = true;
 
-    xdg.enable = true;
 
-    #enable Modules
-    modules = {
-        browser.firefox.enable = true;
+  xdg = {
+    enable = true;
+  
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      setSessionVariables = true;
 
-        desktop.kdeplasma.enable = true;
-        desktop.thunderbird.enable = true;
-
-        development = {
-        vscode.enable = true;
-        docker.enable = false;
-        claude.enable = true;
-        };
-
-        terminal = {
-        kitty.enable = true;
-        };
-        
-        gaming.enable = true;
+      desktop     = "${config.home.homeDirectory}/Desktop";
+      download    = "${config.home.homeDirectory}/Downloads";
+      documents   = "${config.home.homeDirectory}/Dokumente";
+      music       = "${config.home.homeDirectory}/Musik";
+      pictures    = "${config.home.homeDirectory}/Bilder";
+      videos      = "${config.home.homeDirectory}/Videos";
+      templates   = "${config.home.homeDirectory}/Vorlagen";
+      publicShare = "${config.home.homeDirectory}/Öffentlich";
     };
+
+    configFile."wget/wgetrc".text = ''
+      hsts-file = ${config.xdg.cacheHome}/wget-hsts
+    '';
+
+    configFile."conda/condarc".text = ''
+      envs_dirs:
+        - ${config.xdg.dataHome}/conda/envs
+      pkgs_dirs:
+        - ${config.xdg.cacheHome}/conda/pkgs
+    '';
+  };
+
+
+  #enable Modules
+  modules = {
+      browser.firefox.enable = true;
+
+      desktop.kdeplasma.enable = true;
+      desktop.thunderbird.enable = true;
+
+      development = {
+      vscode.enable = true;
+      docker.enable = false;
+      claude.enable = true;
+      };
+
+      terminal = {
+      kitty.enable = true;
+      };
+      
+      gaming.enable = true;
+  };
 
 }
