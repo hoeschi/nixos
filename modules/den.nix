fca14972a5229233648e51859acf23d50e67e256
hoeschi <hoeschbj+github@proton.me>
2026-08-15 21:07:32 +0200

    added xdg to default den

--- modules/den.nix
+++ modules/den.nix
@@ -10,7 +10,7 @@
 
   den.default = {
 
-    nixos = {...}: {
+    nixos = {pkgs, ...}: {
       # Localization
       time.timeZone = "Europe/Berlin";
 
@@ -41,6 +41,43 @@
         backupFileExtension = "hm-bak";
         overwriteBackup = true;
       };
+
+      fonts.packages = with pkgs; [
+        # ttf-ms-fonts
+        # ttf-vista-fonts
+        # ttf-liberation
+        # ttf-dejavu
+        # ttf-roboto
+        # ttf-fira-code
+        # ttf-jetbrains-mono
+        corefonts
+        vista-fonts
+        #nerd-fonts
+      ];
+
+      # XDG Base Directory Specification – systemweit für alle Sessions.
+      # Wird via PAM (/etc/pam/environment) gesetzt, gilt also auch für
+      # grafische Logins über SDDM, nicht nur für Login-Shells.
+      environment.sessionVariables = {
+        XDG_CACHE_HOME  = "$HOME/.cache";
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_DATA_HOME   = "$HOME/.local/share";
+        XDG_STATE_HOME  = "$HOME/.local/state";
+        PLATFORMIO_CORE_DIR = "$HOME/.local/share/platformio";
+        DOTNET_CLI_HOME     = "$HOME/.local/share/dotnet";
+
+      };
+
+      # Verzeichnisse deklarativ anlegen, statt sich darauf zu verlassen,
+      # dass jedes Programm das selbst tut. %h = Home des jeweiligen Users.
+      systemd.user.tmpfiles.rules = [
+        "d %h/.cache        0700 - - -"
+        "d %h/.config       0755 - - -"
+        "d %h/.local        0755 - - -"
+        "d %h/.local/share  0755 - - -"
+        "d %h/.local/state  0755 - - -"
+      ];
+
     };
   };
 
