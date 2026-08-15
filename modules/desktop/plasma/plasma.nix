6230c59e88e63a3ed44075372d033e67e2e68a6d
hoeschi <hoeschbj+github@proton.me>
2026-08-15 16:14:24 +0200

    added plasma to den config

---
+++ modules/desktop/plasma/plasma.nix
@@ -0,0 +1,23 @@
+{den, ...}:
+{
+  den.aspects.gui.provides.plasma = {
+
+    
+    nixos = {pkgs, ...}:{
+        services.desktopManager.plasma6.enable = true;
+  
+      environment.systemPackages = with pkgs; [
+        kdePackages.plasma-browser-integration
+        kdePackages.kwallet
+        kdePackages.kclock
+      ];
+
+      xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [
+            kdePackages.xdg-desktop-portal-kde
+        ];
+    };
+    };
+  };
+}
