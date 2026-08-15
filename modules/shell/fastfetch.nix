6036704743ad34a8a56714c04808d212fcd80ca6
hoeschi <hoeschbj+github@proton.me>
2026-08-15 17:50:55 +0200

    added fastfetch to den

---
+++ modules/shell/fastfetch.nix
@@ -0,0 +1,44 @@
+{
+  den,
+  ...
+}:
+{
+  den.aspects.shell.provides.fastfetch = {
+
+    homeManager = {pkgs, config,...}:
+    {
+      programs.fastfetch = {
+        enable = true;
+        settings = {
+          logo = {
+            padding = {
+              top = 1;
+              left = 2;
+            };
+          };
+          display = {
+            separator = "  ";
+          };
+          modules = [
+            "title"
+            "separator"
+            "os"
+            "kernel"
+            "uptime"
+            "packages"
+            "shell"
+            #"de"
+            "wm"
+            "terminal"
+            "cpu"
+            "gpu"
+            "memory"
+            "disk"
+            "break"
+            "colors"
+          ];
+        };
+      };
+    };
+  };
+}
