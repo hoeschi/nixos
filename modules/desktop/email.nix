4781355a4d9886879818a907cb0cebdc9e89bc5d
hoeschi <hoeschbj+github@proton.me>
2026-08-15 21:44:46 +0200

    initial commit of email.nix in den

---
+++ modules/desktop/email.nix
@@ -0,0 +1,29 @@
+{
+  den,
+  ...
+}:
+{
+  den.aspects.email = {
+
+    homeManager = {pkgs, ...}:
+    {
+      services.protonmail-bridge.enable = true;
+
+      programs.thunderbird = {
+        
+        enable = true;
+
+        profiles = {
+          "default" = {
+            isDefault = true;
+            settings = {
+
+            };
+          };
+        };
+        
+      };
+
+    };
+  };
+}
