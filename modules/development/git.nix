2e2ea3ea25af260d722c7476b340278fe6216022
hoeschi <hoeschbj+github@proton.me>
2026-08-15 20:09:18 +0200

    added git.nix to den

---
+++ modules/development/git.nix
@@ -0,0 +1,87 @@
+{den, ...}:
+{
+
+  den.aspects.development.provides.git = {
+
+    homeManager = {pkgs, ...}:
+    {
+
+      programs.git = {
+
+        enable = true;
+        #userName = "hoeschi";
+        #userEmail = "hoeschbj+github@proton.me";
+
+        lfs.enable = true;
+
+        settings = {
+
+          user = {
+            name = "hoeschi";
+            email = "hoeschbj+github@proton.me";
+
+          };
+
+
+          core.editor = "nvim";
+          pull.rebase = false;
+          init.defaultBranch = "main";
+
+          url."git@github.com:".insteadOf = "https://github.com/";
+
+          core.pager = "diff-so-fancy | less --tabs=4 -RFX";
+          interactive.diffFilter = "diff-so-fancy --patch";
+
+          color = {
+            ui = true;
+            diff-highlight = {
+              oldNormal = "red bold";
+              oldHighlight = "red bold 52";
+              newNormal = "green bold";
+              newHighlight = "green bold 22";
+            };
+            diff = {
+              meta = "11";
+              frag = "magenta bold";
+              func = "146 bold";
+              commit = "yellow bold";
+              old = "red bold";
+              new = "green bold";
+              whitespace = "red reverse";
+            };
+          };
+        };
+      };
+
+      programs.lazygit = {
+        enable = true;
+        settings = {
+          gui = {
+            showIcons = true;
+            theme = {
+              lightTheme = false;
+              activeBorderColor = [ "green" "bold" ];
+              inactiveBorderColor = [ "white" ];
+              selectedLineBgColor = [ "blue" ];
+            };
+          };
+          git = {
+            diffRenderers = [
+              {
+                colorArg = "always";
+                command = "diff-so-fancy";
+              }
+            ];
+          };
+        };
+      };
+
+      home.sessionVariables.LAZYGIT_NEW_DIR_FILE = "${config.xdg.cacheHome}/lazygit/newdir";
+      home.packages = with pkgs; [
+        diff-so-fancy
+      ];
+
+    };
+
+  };
+}
