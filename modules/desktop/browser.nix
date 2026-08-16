39c13b7779c84a32e37dba5a5c148c6f1f675cd2
hoeschi <hoeschbj+github@proton.me>
2026-08-16 23:16:13 +0200

    added global theme to firefox

--- modules/desktop/browser.nix
+++ modules/desktop/browser.nix
@@ -7,19 +7,25 @@
 
         profiles.default = {
           # Konfiguration
-          extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-            ublock-origin
-            onepassword-password-manager
-            darkreader
-            seventv
-            #new-tab-override
-          ];
+          extensions = {
+            packages = with pkgs.nur.repos.rycee.firefox-addons; [
+              ublock-origin
+              onepassword-password-manager
+              darkreader
+              seventv
+              #new-tab-override
+            ];
+
+            # Stylix schreibt hier das Firefox-Color-Theme rein
+            settings."FirefoxColor@mozilla.com".force = true;
+          };
         };
       };
 
-      stylix.targets = {
+      stylix.targets.firefox = {
         #firefox.profileNames = lib.attrNames (config.programs.firefox.profiles or {});
-        firefox.profileNames = ["default"];
+        profileNames = ["default"];
+        colorTheme.enable = true;
       };
     };
   };
