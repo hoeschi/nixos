645f1f7ea86cad290c2f7d0e9ef6cc5de63c2070
hoeschi <hoeschbj+github@proton.me>
2026-08-16 19:40:52 +0200

    added sansserif for dunst

--- modules/desktop/hyprland/stylix.nix
+++ modules/desktop/hyprland/stylix.nix
@@ -17,20 +17,29 @@
 
         # Passt zu dem, was du aktuell in kitty.nix hardcodiert hast
         base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+        polarity = "dark";
 
         fonts = {
           monospace = {
             package = pkgs.nerd-fonts.hack;
             name = "Hack Nerd Font Mono";
           };
-          # serif/sansSerif bleiben auf DejaVu-Default, bis du was Eigenes willst
+
+          # for dunst
+          sansSerif = {
+            package = pkgs.inter;
+            name = "Inter";
+          };
 
           sizes = {
             # terminal = 10.5; # → 14 px im Editor, entspricht deinem alten Wert
             applications = 10;
             terminal = 11;
+            popups = 11;
           };
         };
+
+        opacity.popups = 0.9;
 
         cursor = {
           package = pkgs.bibata-cursors;
