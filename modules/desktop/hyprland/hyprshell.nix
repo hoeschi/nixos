be34d0b0e8e7893346a7b21e390f8d733eba528d
hoeschi <hoeschbj+github@proton.me>
2026-08-17 17:05:02 +0200

    added style

--- modules/desktop/hyprland/hyprshell.nix
+++ modules/desktop/hyprland/hyprshell.nix
@@ -1,19 +1,44 @@
-{den, ...}: {
+{
+  den,
+  lib,
+  ...
+}: {
   den.aspects.gui.provides.hyprland = {
-    homeManager = {
+    homeManager = {config, ...}: {
       services.hyprshell = {
         enable = true;
+
         settings = {
+          version = 4;
           windows = {
             scale = 8.0;
             items_per_row = 5;
+            overview = {
+              key = "tab";
+              modifier = "super";
+              filter_by = [];
+            };
             switch = {
               modifier = "alt";
               filter_by = [];
-              switch_workspaces = false;
+              switch_workspaces = false; # switch apps instead of workspaces
             };
           };
         };
+
+        style = let
+          c = config.lib.stylix.colors.withHashtag;
+        in ''
+          :root {
+            --bg-color: ${c.base00};
+            --bg-color-hover: ${c.base02};
+            --border-color: ${c.base03};
+            --border-color-active: ${c.base0D};
+            --index-border-color: ${c.base0D};
+            --border-radius: 12px;
+            --border-size: 2px;
+          }
+        '';
       };
     };
   };
