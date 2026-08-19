2e5da9d73ad831a6dd1c20b359d9c069759cdba9
hoeschi <hoeschbj+github@proton.me>
2026-08-19 18:36:43 +0200

    added base.nix

---
+++ modules/desktop/base.nix
@@ -0,0 +1,18 @@
+{den, ...}: {
+  den.aspects.gui.provides.wayland-base = {
+    includes = [
+      den.aspects.shell.provides.kitty
+    ];
+
+    homeManager = {pkgs, ...}: {
+      home.packages = with pkgs; [
+        libnotify
+
+        grim
+        slurp
+
+        wl-clipboard # for Screenshot functionality
+      ];
+    };
+  };
+}
